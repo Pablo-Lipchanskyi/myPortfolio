a06698be1001159ea7dee2d8bb9eae3af16df3d7
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-03-30 13:11:31 +0300

    comm

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return (
+        <>
+            <h3>My Portfolio</h3>
+            <nav>
+                <li>Home</li>
+                <li>CV</li>
+                <li>Contacts</li>
+            </nav>
+        </>
+    )
+}
