58d08e8f349dbd458e5c27edf856f85ec82291be
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-04-11 11:26:36 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,7 @@
 export const Header = () => {
     return (
         <>
-            <h3>My Portfolio</h3>
-            <nav>
-                <li>Home</li>
-                <li>CV</li>
-                <li>Contacts</li>
-            </nav>
+            <b>My Portfolio</b>
         </>
     )
 }
